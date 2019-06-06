d5404af10dba9bb8f74bd36d26bd299c5760ed6e
Pham Van Chien <phamvanbk11@gmail.com>
2019-06-06 10:58:10 +0700

    signup, login, logout

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+
+//= require Web/bootstrap.min.js
+//= require Web/jquery.min.js
+//= require Web/jquery.zoom.min.js
+//= require Web/main.js
+//= require Web/nouislider.min.js
+//= require Web/slick.min.js
